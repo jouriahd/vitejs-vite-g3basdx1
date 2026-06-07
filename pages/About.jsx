c7507b2fd45bede069d511dd5dc344953b42d79e
jouriahd <jouri.saidd@gmail.com>
2026-06-07 04:39:01 +0300

    Update About.jsx

--- pages/About.jsx
+++ pages/About.jsx
@@ -2,7 +2,7 @@ function About() {
   return (
     <div style={{ padding: "20px" }}>
       <h1 color:pink>About TaskTracker </h1>
-      <h2 color:purpule>This application is built using React.</h2>
+      <h2 color:purpule>This application is built using React  by a developer who hate web programming .</h2>
     </div> 
   );
 }
