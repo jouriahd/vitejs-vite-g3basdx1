4ba8f9b4ca1fce8fccb05d9281bda6f1b6d15972
jouriahd <jouri.saidd@gmail.com>
2026-06-07 04:39:45 +0300

    Update Home.jsx

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -2,7 +2,7 @@ function Home() {
   return (
     <div style={{ padding: "20px" }}>
       <h1 color:pink>Welcome to TaskTracker</h1>
-      <h2 color:purple >Organize, track, and complete your daily tasks</h2>
+      <h2 color:purple >Organize, track, and complete your daily tasks to keep your beautiful life orgnized</h2>
     </div>
   );
 }
